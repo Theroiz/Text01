2ddb0f2f6a896a34aff8b33f2a8dd2bacd35de2d
Theroiz <164636377+Theroiz@users.noreply.github.com>
2024-06-06 21:36:18 -0300

    Add files via upload
    exercicio de texto

---
+++ src/Text01.java
@@ -0,0 +1,13 @@
+public class Text01 {
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\   \n"+" /                \\ \n"+"(  /\\         /\\  )\n"+"====     V     ==== \n"+"======(__|__)======\n"+"  (             )  \n"+"   (___________)   ");
+        //System.out.println("  /  \\_______/  \\  ");
+        //System.out.println(" /                \\ ");
+        //System.out.println("(  /\\         /\\  )");
+        //System.out.println("====     V     ====");
+        //System.out.println("======(__|__)======");
+        //System.out.println("  (             )  ");
+        //System.out.println("   (___________)   ");
+       
+    }    
+}
